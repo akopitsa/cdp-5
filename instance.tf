0bbb40d1bf41f6141943438b80382e56a87f0018
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-10-18 14:53:02 +0300

    Terraform AWS Puppet Server & Agent

--- instance.tf
+++ instance.tf
@@ -13,13 +13,41 @@ resource "aws_instance" "puppet-server" {
     }
     vpc_security_group_ids = ["${aws_security_group.puppet-server.id}"]
     key_name = "${aws_key_pair.my_public_ssh_key.key_name}"
-    connection {
-      user = "${var.INSTANCE_USERNAME}"
-    }
+    # connection {
+    #   user = "${var.INSTANCE_USERNAME}"
+    # }
     depends_on = ["aws_security_group.puppet-server"]
     user_data = "${file("server_install.sh")}"
     tags {
       Name = "PuppetServer"
+    }
+}
+
+data "template_file" "puppet-agent" {
+  template = "${file("agent_install.sh")}"
+  vars {
+    dns_name = "${aws_instance.puppet-server.public_dns}"
+  }
+}
+
+resource "aws_instance" "puppet-agent" {
+    ami = "ami-ae7bfdb8"
+    instance_type = "t2.micro"
+    availability_zone = "${var.AVAILABILITY_ZONE}"
+    root_block_device {
+      volume_size = "8"
+      delete_on_termination = true
+    }
+    vpc_security_group_ids = ["${aws_security_group.puppet-agent.id}"]
+    key_name = "${aws_key_pair.my_public_ssh_key.key_name}"
+    # connection {
+    #   user = "${var.INSTANCE_USERNAME}"
+    # }
+    depends_on = ["aws_security_group.puppet-agent"]
+    #user_data = "${file("agent_install.sh")}"
+    user_data = "${data.template_file.puppet-agent.rendered}"
+    tags {
+      Name = "PuppetAgent"
     }
 }
 
@@ -46,4 +74,30 @@ resource "aws_security_group" "puppet-server" {
     tags {
       Name = "ASG-for-PuppetServer"
     }
+}
+
+resource "aws_security_group" "puppet-agent" {
+    name = "ASG-for-PuppetAgent"
+    ingress {
+      from_port = "${var.SSH-PORT}"
+      to_port = "${var.SSH-PORT}"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags {
+      Name = "ASG-for-PuppetAgent"
+    }
+}
+
+output "puppet_server_dns" {
+  value = "${aws_instance.puppet-server.public_dns}"
+}
+output "puppet_agent_public_dns" {
+  value = "${aws_instance.puppet-agent.public_dns}"
 }
--- server_install.sh
+++ server_install.sh
@@ -1,23 +1,61 @@
 #!/bin/bash
 
-sudo dd if=/dev/zero of=/swapfile bs=1M count=5120
-sudo chown root:root  /swapfile
-sudo chmod 600 /swapfile
+dd if=/dev/zero of=/swapfile bs=1M count=2560
+chown root:root  /swapfile
+chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
-sudo echo "/swapfile   swap    swap    sw  0   0" >> /etc/fstab
-sudo echo "vm.swappiness = 100" >> /etc/sysctl.conf
-sudo echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
-sudo sysctl -p
-sudo yum update
-sudo yum install -y ntp vim mc git tree net-tools
-sudo timedatectl set-timezone Europe/Kiev
-sudo ntpdate pool.ntp.org
-sudo sed -i 's/server 0.centos.pool.ntp.org/server 0.ua.pool.ntp.org/g' /etc/ntp.conf
-sudo sed -i 's/server 1.centos.pool.ntp.org/server 1.ua.pool.ntp.org/g' /etc/ntp.conf
-sudo sed -i 's/server 2.centos.pool.ntp.org/server 2.ua.pool.ntp.org/g' /etc/ntp.conf
-sudo sed -i 's/server 3.centos.pool.ntp.org/server 3.ua.pool.ntp.org/g' /etc/ntp.conf
-sudo systemctl restart ntpd
-sudo systemctl enable ntpd
-sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-sudo yum -y install puppetserver
+echo "/swapfile   swap    swap    sw  0   0" >> /etc/fstab
+echo "vm.swappiness = 100" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+sysctl -p
+yum update
+yum install -y ntp vim mc git tree net-tools
+timedatectl set-timezone Europe/Kiev
+ntpdate pool.ntp.org
+sed -i 's/server 0.centos.pool.ntp.org/server 0.ua.pool.ntp.org/g' /etc/ntp.conf
+sed -i 's/server 1.centos.pool.ntp.org/server 1.ua.pool.ntp.org/g' /etc/ntp.conf
+sed -i 's/server 2.centos.pool.ntp.org/server 2.ua.pool.ntp.org/g' /etc/ntp.conf
+sed -i 's/server 3.centos.pool.ntp.org/server 3.ua.pool.ntp.org/g' /etc/ntp.conf
+systemctl restart ntpd
+systemctl enable ntpd
+rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+yum -y install puppetserver
+echo "autosign = true" >> /etc/puppetlabs/puppet/puppet.conf
+SERVERHOSTNAME=`/usr/bin/curl http://169.254.169.254/latest/meta-data/public-hostname` && \
+echo "dns_alt_names = $SERVERHOSTNAME,server" >> /etc/puppetlabs/puppet/puppet.conf
+sed -i -e 's/JAVA_ARGS="-Xms2g -Xmx2g -XX:MaxPermSize=256m"/JAVA_ARGS="-Xms512m -Xmx512m"/' "/etc/sysconfig/puppetserver"
+SERVERHOSTNAME=`/usr/bin/curl http://169.254.169.254/latest/meta-data/public-hostname` && \
+echo "[main]" >> /etc/puppetlabs/puppet/puppet.conf && \
+echo "certname =  $SERVERHOSTNAME" >> /etc/puppetlabs/puppet/puppet.conf && \
+echo "server =  $SERVERHOSTNAME" >> /etc/puppetlabs/puppet/puppet.conf && \
+echo "environment = production" >> /etc/puppetlabs/puppet/puppet.conf && \
+echo "runinterval = 2m" >> /etc/puppetlabs/puppet/puppet.conf
+echo """:backends:
+  - yaml
+:hierarchy:
+  - 'nodes/%{::trusted.certname}'
+  - 'nodes/%{::fqdn}'
+  - common
+
+:yaml:
+# datadir is empty here, so hiera uses its defaults:
+# - /etc/puppetlabs/code/environments/%{environment}/hieradata on *nix
+# When specifying a datadir, make sure the directory exists.
+  :datadir: '/etc/puppetlabs/code/environments/%{::environment}/hieradata'
+""" > /etc/puppetlabs/puppet/hiera.yaml
+/opt/puppetlabs/puppet/bin/gem install r10k
+echo ''':cachedir: '/var/cache/r10k'
+:sources:
+  cdp:
+    remote: 'https://github.com/akopitsa/cdp-control-repo.git'
+    basedir: '/etc/puppetlabs/code/environments'
+    prefix: false
+''' >> /etc/puppetlabs/puppet/r10k.yaml
+
+/opt/puppetlabs/puppet/bin/r10k deploy environment production -pv -c /etc/puppetlabs/puppet/r10k.yaml
+/opt/puppetlabs/puppet/bin/r10k deploy environment development -pv -c /etc/puppetlabs/puppet/r10k.yaml
+
+free && sync && echo 3 > /proc/sys/vm/drop_caches && free
+systemctl start puppetserver
+systemctl enable puppetserver
